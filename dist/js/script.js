ce6eb3da68421b9bbc81da49aa16ce9f4c4e28cf
napoleones1 <muhamadhaikal.me@gmail.com>
2024-03-14 18:40:01 +0700

    first

--- dist/js/script.js
+++ dist/js/script.js
@@ -36,23 +36,71 @@ window.addEventListener ('click', function(e) {
 })
 
 //darkmode toggle
-const darkToggle = document.querySelector('#dark-toggle');
-const html = document.querySelector('html');
+//const darkToggle = document.querySelector('#dark-toggle');
+//const html = document.querySelector('html');
+//
+//darkToggle.addEventListener('click', function() {
+//    if (darkToggle.checked) {
+//        html.classList.add('dark');
+//        localStorage.theme = 'dark';
+//    } else {
+//        html.classList.remove('dark');
+//        localStorage.theme = 'light'
+//    }
+//})
+//
+//
+////memindahkan posisi toggle dark mode
+//if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+//    darkToggle.checked = true;
+//  } else {
+//    darkToggle.checked = false;
+//  }
 
-darkToggle.addEventListener('click', function() {
-    if (darkToggle.checked) {
-        html.classList.add('dark');
-        localStorage.theme = 'dark';
-    } else {
-        html.classList.remove('dark');
-        localStorage.theme = 'light'
+//icon
+const sunIcon = document.querySelector(".sun");
+const moonIcon = document.querySelector(".moon");
+
+//them vars
+const userTheme = localStorage.getItem('theme');
+const systemTheme = window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+//icon toggling
+const iconToggle = () => {
+    moonIcon.classList.toggle('display-none');
+    sunIcon.classList.toggle('display-none');
+};
+
+//inithial them check
+const themeCheck = () => {
+    if (userTheme === 'dark' || (!userTheme && systemTheme)) {
+        document.documentElement.classList.add('dark');
+        moonIcon.classList.add('display-none');
+        return
     }
-})
+    sunIcon.classList.add('display-none');
+};
 
+//manual them switch
+const themeSwitch = () => {
+    if (document.documentElement.classList.contains('dark')){
+        document.documentElement.classList.remove('dark');
+        localStorage.setItem('theme', 'light');
+        iconToggle();
+        return;
+    }
+    document.documentElement.classList.add('dark');
+        localStorage.setItem('theme', 'dark');
+        iconToggle();
+};
 
-//memindahkan posisi toggle dark mode
-if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-    darkToggle.checked = true;
-  } else {
-    darkToggle.checked = false;
-  }
+//call theme switch on clicking buttons
+sunIcon.addEventListener('click', () => {
+    themeSwitch();
+});
+moonIcon.addEventListener('click', () => {
+    themeSwitch();
+});
+
+//invoke theme check to initial load
+themeCheck();
